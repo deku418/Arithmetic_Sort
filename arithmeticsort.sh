b044a7630f2b3aae0c2cf98c2de645ccd899ee4b
deku418 <mayurvarade397@gmail.com>
2021-03-30 07:08:00 +0530

    Add_compute1

--- arithmeticsort.sh
+++ arithmeticsort.sh
@@ -5,5 +5,7 @@ echo "Welcome to Arithmetic Computation and Sorting"
 read -p "Enter a : " a
 read -p "Enter b : " b
 read -p "Enter c : " c
-
 echo "a=$a  b=$b  c=$c"
+
+result1=$((a+b*c))
+echo "$a+$b*$c =$result1"
